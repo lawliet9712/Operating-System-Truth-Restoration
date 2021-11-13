59247f4d3d25ceb41eac0f004329d2881718353f
lawliet9 <lawliet9@gitee.com>
2021-11-13 11:44:46 +0800

    修复设置页表时错误问题:因为 loader 过大,写入到 hd60M.img 时,有代码被截断,故修改 build_loader 脚本,将块数调整为 3

--- L-OS/code/build_loader.sh
+++ L-OS/code/build_loader.sh
@@ -1,5 +1,5 @@
 echo "start build loader ..."
 NAME="loader"
 nasm -o ../bin/$NAME.bin ./boot/$NAME.S -I ./include/
-dd if=../bin/$NAME.bin of=$HOME/study/Operating-System-Truth-Restoration/L-OS/hd60M.img bs=512 count=2 conv=notrunc seek=2
+dd if=../bin/$NAME.bin of=$HOME/study/Operating-System-Truth-Restoration/L-OS/hd60M.img bs=512 count=3 conv=notrunc seek=2
 echo "finish ..."
