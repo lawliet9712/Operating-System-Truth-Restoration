401f48a9273ef96b66afe5b621a7d511466f7dfe
lawliet9 <www.wwj19@qq.com>
2021-11-21 00:17:28 +0800

    1. 完善中断,补充 C 中断处理接口

--- L-OS/code/build_kernel.sh
+++ L-OS/code/build_kernel.sh
@@ -1,9 +1,10 @@
 nasm -f elf -o build/print.o lib/kernel/print.S
 nasm -f elf -o build/kernel.o kernel/kernel.S
+$HOME/local/bin/gcc -I lib/kernel/ -fno-builtin -m32 -fno-stack-protector -c -o build/timer.o device/timer.c
 $HOME/local/bin/gcc -I lib/kernel/ -I kernel/ -I lib/ -fno-builtin -m32 -fno-stack-protector -c -o build/main.o kernel/main.c
 $HOME/local/bin/gcc -I lib/kernel/ -I kernel/ -I lib/ -fno-builtin -m32 -fno-stack-protector -c -o build/interrupt.o kernel/interrupt.c
 $HOME/local/bin/gcc -I lib/kernel/ -I kernel/ -I lib/ -fno-builtin -m32 -fno-stack-protector -c -o build/init.o kernel/init.c
 # 设定地址为内核空间 0x1500
-ld -m elf_i386 -Ttext 0xc0001500 -e main -o build/kernel.bin build/main.o build/init.o build/interrupt.o build/print.o build/kernel.o
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o build/kernel.bin build/main.o build/init.o build/interrupt.o build/print.o build/kernel.o build/timer.o
 # 写入到硬盘, 第 9  个扇区写入,一次性写入 200, 这样后续 kernel 变大也不用怎么调整
 dd if=build/kernel.bin of=$HOME/study/Operating-System-Truth-Restoration/L-OS/hd60M.img bs=512 count=200 seek=9 conv=notrunc
--- L-OS/code/build_loader.sh
+++ L-OS/code/build_loader.sh
@@ -1,5 +1,5 @@
 echo "start build loader ..."
 NAME="loader"
-nasm -o ../bin/$NAME.bin ./boot/$NAME.S -I ./include/
+nasm -o ../bin/$NAME.bin ./boot/$NAME.S -I ./boot/include/
 dd if=../bin/$NAME.bin of=$HOME/study/Operating-System-Truth-Restoration/L-OS/hd60M.img bs=512 count=3 conv=notrunc seek=2
 echo "finish ..."
