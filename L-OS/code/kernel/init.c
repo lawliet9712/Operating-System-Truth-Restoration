c7e5223aed69c949fe83cbed2ece89fd1c83464b
lawliet9 <www.wwj19@qq.com>
2021-11-21 15:27:00 +0800

    完善内存管理

--- L-OS/code/kernel/init.c
+++ L-OS/code/kernel/init.c
@@ -1,11 +1,13 @@
 #include "init.h"
 #include "print.h"
-#include "../device/timer.h"
+#include "timer.h"
 #include "interrupt.h"
+#include "memory.h"
 
 /*负责初始化所有模块 */
 void init_all() {
    put_str("init_all\n");
    idt_init();   //初始化中断
    timer_init();
+   mem_init();
 }
